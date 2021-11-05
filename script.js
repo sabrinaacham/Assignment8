006c482e8ef2e4426bc12df1e489cd038da7d4e3
sabrinaacham <sabrinaacham@gmail.com>
2021-11-05 17:49:20 -0400

    move ting arong!

---
+++ script.js
@@ -0,0 +1,6 @@
+var vaccinated = true;
+var name = 'Hello There! ';
+var message = name + 'Please present your vaccination card upon entry';
+
+var element = document.getElementById('tea');
+element.textContent=message;
